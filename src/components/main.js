a91411811830dc9a4cdc574c80878c21954f531e
brooktm <brooktm@gmail.com>
2018-12-10 14:33:10 -0800

    intial site structure done using react-mdl

---
+++ src/components/main.js
@@ -0,0 +1,12 @@
+// import React from "react";
+
+// import Landing from "./landingpage";
+// import { Switch, Route } from "react-router-dom";
+
+// const Main = () => (
+//   <Switch>
+//     <Route exact path="/" components={Landing} />
+//   </Switch>
+// );
+
+// export default Main;
---
+++ src/components/projects.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Projects extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Projets Page</h1>
+      </div>
+    );
+  }
+}
+
+export default Projects;
