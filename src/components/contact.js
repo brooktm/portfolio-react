2067609ca385c380623615faec472295aa2741d2
brooktm <brooktm@gmail.com>
2018-12-26 13:01:42 -0800

    contact form with php backend setup

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,34 @@
 import React, { Component } from "react";
+import axios from "axios";
 
+const API_PATH =
+  "http://localhost:3000/react-contact-form/api/contact/index.php";
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: "",
+      lname: "",
+      email: "",
+      message: "",
+      mailSent: false,
+      error: null
+    };
+  }
+  handleFormSubmit(e) {
+    e.preventDefault();
+    axios({
+      method: "post",
+      url: `${API_PATH}`,
+      headers: { "content-type": "application/json" },
+      data: this.state
+    })
+      .then(result => {
+        this.setState({ mailSent: result.data.sent });
+      })
+      .catch(error => this.setState({ error: error.message }));
+  }
+
   render() {
     return (
       <div className="contact-section">
@@ -13,6 +41,8 @@ class Contact extends Component {
               id="fname"
               name="firstname"
               placeholder="Your name.."
+              value={this.state.fname}
+              onChange={e => this.setState({ fname: e.target.value })}
             />
             <label>Last Name</label>
             <input
@@ -20,6 +50,8 @@ class Contact extends Component {
               id="lname"
               name="lastname"
               placeholder="Your last name.."
+              value={this.state.lname}
+              onChange={e => this.setState({ lname: e.target.value })}
             />
 
             <label>Email</label>
@@ -28,6 +60,8 @@ class Contact extends Component {
               id="email"
               name="email"
               placeholder="Your email"
+              value={this.state.email}
+              onChange={e => this.setState({ email: e.target.value })}
             />
 
             <label>Subject</label>
@@ -35,8 +69,19 @@ class Contact extends Component {
               id="subject"
               name="subject"
               placeholder="Write something.."
+              onChange={e => this.setState({ message: e.target.value })}
+              value={this.state.message}
             />
-            <input type="submit" value="Submit" />
+            <input
+              type="submit"
+              value="Submit"
+              onClick={e => this.handleFormSubmit(e)}
+              value="Submit"
+            />
+
+            <div>
+              {this.state.mailSent && <div>Thank you for contcting us.</div>}
+            </div>
           </form>
         </div>
       </div>
